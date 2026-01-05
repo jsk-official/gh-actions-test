37d16c10678326a7e9acaf427d8ed82979c7092e
JSK_Official <151975302+jsk-official@users.noreply.github.com>
2026-01-05 01:47:00 -0800

    Refactor commit message webhook to use structured format

--- scripts/commit_message_webhook.py
+++ scripts/commit_message_webhook.py
@@ -5,11 +5,28 @@ import json
 events = open(os.environ["GITHUB_EVENT_PATH"], "r")
 parsed_events = json.loads(events.read())
 
-message_content = ""
+commits = {}
+cnt = 1
+
+commits["fields"] = []
+
 
 for i in parsed_events["commits"]:
-   message_content += i['message'] + ' - ' + i['author']['name'] + '\n'
+   commits["fields"].append({
+      "name": "#" + cnt + "by " + i["author"]["name"],
+      "value": i["message"]
+   })
+
+   cnt += 1
+
+commits["footer"] = {
+   "text": "Branch: unknown"
+}
+
+print(commits)
 
 requests.post(os.environ["COMMITS_CHANNEL_WEBHOOK"], {
-   "content": message_content
+   "username": "Armored Patrol Remastered Changelogs",
+   "avatar_url": "https://cdn.discordapp.com/icons/1021084114343952484/db5194b83958a75d14cf2e84a715cddb.webp?size=256&quality=lossless",
+   "embeds": commits,
 })
