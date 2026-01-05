bb47b815b7fa6e33445b591128e0972c2e916168
JSK_Official <151975302+jsk-official@users.noreply.github.com>
2026-01-05 01:33:34 -0800

    Fix commit message webhook to use author variable

--- scripts/commit_message_webhook.py
+++ scripts/commit_message_webhook.py
@@ -1,11 +1,10 @@
 import os
 import requests
 
-print(os.environ["commitmsg"])
 print(os.environ["author"])
 
 print(os.environ["GITHUB_EVENT_PATH"])
 
 requests.post(os.environ["COMMITS_CHANNEL_WEBHOOK"], {
-   "content": os.environ["commitmsg"] + " - " + os.environ["author"]
+   "content": os.environ["author"] + " - " + os.environ["author"]
 })
