ba4aca853e2e45a7906693b65427297a8dc6729d
JSK_Official <151975302+jsk-official@users.noreply.github.com>
2026-01-05 01:54:30 -0800

    Refactor commit message webhook for embeds

--- scripts/commit_message_webhook.py
+++ scripts/commit_message_webhook.py
@@ -5,29 +5,32 @@ import json
 events = open(os.environ["GITHUB_EVENT_PATH"], "r")
 parsed_events = json.loads(events.read())
 
-commits = {}
-cnt = 0
+commits_embed = {}
+cnt = 1
 
-commits["fields"] = []
+commits_embed["fields"] = []
 
 for i in parsed_events["commits"]:
    commits["fields"].append({
-      "name": "#" + str(cnt + 1) + "by " + i["author"]["name"],
+      "name": "#" + str(cnt) + "by " + i["author"]["name"],
       "value": i["message"]
    })
 
    cnt += 1
 
-commits["footer"] = {
-   "text": "Branch: unknown"
-}
+commits_embed["title"] = cnt + "new commit"
+
+if cnt > 1:
+   commits_embed["title"] += "s"
+
+commits_embed["description"] = "Branch: Unknown"
 
 print(commits)
 
 response = requests.post(os.environ["COMMITS_CHANNEL_WEBHOOK"], {
    "username": "Armored Patrol Remastered Changelogs",
    "avatar_url": "https://cdn.discordapp.com/icons/1021084114343952484/db5194b83958a75d14cf2e84a715cddb.webp?size=256&quality=lossless",
-   "embeds": commits,
+   "embeds": commits_embed,
 })
 
 print(response.text)
