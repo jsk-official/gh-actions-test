da0b1f1fc3d2627a60e94598561117e65f7b5690
JSK_Official <151975302+jsk-official@users.noreply.github.com>
2026-01-05 01:24:29 -0800

    Implement commit message webhook notification
    Add functionality to send commit messages to a webhook.

--- scripts/commit_message_webhook.py
+++ scripts/commit_message_webhook.py
@@ -1,4 +1,9 @@
 import os
+import requests
 
 print(os.environ["commitmsg"])
 print(os.environ["author"])
+
+requests.post(os.environ["COMMITS_CHANNEL_WEBHOOK"], {
+   "content": os.environ["commitmsg"] + " - " + os.environ["author"]
+})
