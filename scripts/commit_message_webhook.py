c873d1c5e47d7c25f5f1e6bd2a9aaee0f720440f
JSK_Official <151975302+jsk-official@users.noreply.github.com>
2026-01-05 01:32:24 -0800

    Log GITHUB_EVENT_PATH in commit message webhook
    Added logging for GITHUB_EVENT_PATH environment variable.

--- scripts/commit_message_webhook.py
+++ scripts/commit_message_webhook.py
@@ -4,6 +4,8 @@ import requests
 print(os.environ["commitmsg"])
 print(os.environ["author"])
 
+print(os.environ["GITHUB_EVENT_PATH"])
+
 requests.post(os.environ["COMMITS_CHANNEL_WEBHOOK"], {
    "content": os.environ["commitmsg"] + " - " + os.environ["author"]
 })
